905a6cb71e716840614e3a70b498d23c14edf0d9
Kevin Carter <50137234+kevin9carter@users.noreply.github.com>
2020-03-06 08:18:23 -0700

    Fixed the issue <Netlify>

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/multikart-admin/precache-manifest.5f6574f2737306df0a560e6d620dcdc8.js"
+  "/precache-manifest.f308b015d66ded191b48625d046d4a99.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/multikart-admin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
